6a45bcd937fd4e169b598b3ed430584b893170b2
Timur <sherkhantimur@gmail.com>
2026-06-30 09:49:19 +0500

    nfa updated

---
+++ nfa/nfa.hpp
@@ -0,0 +1,34 @@
+#ifndef LAB2_NFA_HPP
+#define LAB2_NFA_HPP
+
+#include <cstddef>
+#include <vector>
+
+#include "ast.hpp"
+
+struct Transition {
+    size_t from_state;
+    size_t to_state;
+    char transition_symbol;
+    bool is_epsilon;
+};
+
+struct NfaFragment {                                                                    // фрагмент НКА
+    size_t entry_state;
+    size_t exit_state;
+};
+
+class NFA {
+public:
+    size_t newState();
+    void addTransition(size_t from_state, size_t to_state, char transition_symbol);
+    void addEpsilon(size_t from_state, size_t to_state);
+private:
+    std::vector<Transition> transitions;                                                // список переходов
+    NfaFragment build(const AstNode* node);                                             // билдер НКА
+    size_t num_states = 0;
+    size_t start = 0;
+    size_t accept = 0;
+};
+
+#endif //LAB2_NFA_HPP
